a8bf0169b60dc513459143387a0eeb5f5cd24cbc
Flynn <flynnigan.tan@gmail.com>
2017-06-19 23:09:52 -0500

    completed spotify

--- keys.js
+++ keys.js
@@ -1,8 +1,13 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
 exports.twitterKeys = {
   consumer_key: 'WsgEILTFbILvZe9rkgB3RLrZd',
   consumer_secret: 'VElBpw8UTUEqUa8UUIIXhkVNooZwS4K4x9o4tzkqhnBS1ZcNLw',
   access_token_key: '875465106410192896-nvYT07qUHhHe17jK6twYcuR0sZglsGu',
   access_token_secret: 'aqSziL3Sun8DxUBIocX3s1Ps5GeoixhvxcrpBlfXMGx18',
+}
+
+exports.spotifyKeys = {
+	client_id: "6583f73b8a6c4d1abb57ab9f6b2b8e14",
+	client_secret: "65935849333443848f19739a7aa932bd"
 }
--- liri.js
+++ liri.js
@@ -7,26 +7,24 @@ var fs = require("fs");
 var keys = require("./keys.js");
 
 // get npm inquirer
-var inquirer = require("inquirer");
+//var inquirer = require("inquirer");
 
 // get twitter package
 var Twitter = require("twitter");
+
+// get spotify package
+var Spotify = require('node-spotify-api');
 
 // KEYS ------------------------------------------- //
 
 // the code to grab the data from keys.js
 var twitterConsumerKey = keys.twitterKeys.consumer_key;
-console.log("twitterConsumerKey: " + twitterConsumerKey);
-
 var twitterConsumerSecret = keys.twitterKeys.consumer_secret;
-console.log("twitterConsumerSecret: " + twitterConsumerSecret);
-
 var twitterAccessTokenKey = keys.twitterKeys.access_token_key;
-console.log("twitterAccessTokenKey: " + twitterAccessTokenKey);
-
 var twitterAccessTokenSecret = keys.twitterKeys.access_token_secret;
-console.log("twitterAccessTokenSecret: " + twitterAccessTokenSecret);
 
+var spotifyClientID = keys.spotifyKeys.client_id;
+var spotifyClientSecret = keys.spotifyKeys.client_secret;
 // TWITTER CLIENT ------------------------------------------- //
 
 var client = new Twitter({
@@ -38,79 +36,125 @@ var client = new Twitter({
 
 var params = {screen_name: 'orangetxt'};
 
+// SPOTIFY ------------------------------------------- //
+
+var spotify = new Spotify({
+	id: spotifyClientID,
+	secret: spotifyClientSecret
+});
+
 // PROMPT FOR COMMAND ------------------------------------------- //
 
-// - - - - - from list of prompts using inquirer
-/*inquirer.prompt([
-
-	// if no argument entered, then provide list to choose from
-	{
-		type:"list",
-		message:"Pick a command. ",
-		choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
-		name: "command"
-	}
-
-	]).then(function(user){
-
-		// function for chosen command
-	});
-*/
-
 // - - - - - from command line
-var command = process.argv.splice(2).join(" ");
-console.log("command: " + command);
+if (process.argv[2] != null || process.argv[2] != "") {
+	var command = process.argv[2];
+	console.log("command: " + command);
 
-// function for chosen command
-doThisCommand(command);
+	var media = process.argv.splice(3).join(" ");
+	console.log("media: " + media);
 
-// FUNCTION FOR COMMANDS ------------------------------------------- //
+	// functions for chosen command
 
-function doThisCommand(yourCommand) {
 
-	if (yourCommand === "my-tweets") {
+	// FUNCTION FOR COMMANDS ------------------------------------------- //
 
-		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	if (command === "my-tweets") {
 
-			if (error) {
-				console.log(error);
-			}
-
-			else {
-
-				// show last 20 tweets & when they were created
-
-				for (var i in tweets) {
-
-					if (i < 21) {
-					console.log("");
-					console.log("");
-					console.log("");
-					console.log("Tweets By: " + tweets[i].user.screen_name);
-					console.log("Tweet Created: " + tweets[i].created_at);
-					console.log("Tweets: " + tweets[i].text);
-					}
-				}
-
-			}
-		});	
+		twitterCommand(command);
 	}
 
-	else if (yourCommand === "spotify-this-song") {
+	else if (command === "spotify-this-song") {
 
+		if (media == "" || media == null) {
+			media = "Careless Whisper";
+		}
+
+		spotifyCommand(command, media);
 	}
 
 	else if (yourCommand === "movie-this") {
 
+		movieCommand(command, media);
 	}
 
 	else if (yourCommand === "do-what-it-says") {
 
+		doWhatCommand(command);
+
 	}
 
 	else {
-		console.log("That option is unavailable. Please try again.");
+		console.log("That option is unavailable. Please try again and type in a command as your third argument.");
+		console.log("Your options are: ");
+		console.log("==> 'my-tweets'");
+		console.log("==> 'spotify-this-song'");
+		console.log("==> 'movie-this'");
+		console.log("==> 'do-what-it-says'");
 	}
 
+}
+
+
+
+function twitterCommand(yourCommand) {
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+
+		if (error) {
+			console.log(error);
+		}
+
+		else {
+
+			// show last 20 tweets & when they were created
+
+			for (var i in tweets) {
+
+				if (i < 21) {
+				console.log("===============================");
+				console.log("");
+				console.log("");
+				console.log("Tweets By: " + tweets[i].user.screen_name);
+				console.log("Tweet Created: " + tweets[i].created_at);
+				console.log("Tweets: " + tweets[i].text);
+				console.log("");
+				console.log("");
+				}
+			}
+
+		}
+	});	
+		
+}
+
+function spotifyCommand(yourCommand, yourSong) {
+
+	spotify
+		.search({type:'track', query: yourSong, limit: 1}, function(err, response){
+
+			if (err) {
+				return console.log(err);
+			}
+
+			else {
+
+				console.log("===============================");
+				console.log("");
+				console.log("");
+				console.log("Song Title: " + response.tracks.items[0].name);		
+				console.log("Artist: " + response.tracks.items[0].artists[0].name);
+				console.log("Album: " + response.tracks.items[0].album.name);
+				console.log("Song Preview: " + response.tracks.items[0].preview_url);	
+				console.log("");
+				console.log("");	
+			}
+		});
+}
+	
+function movieCommand(yourCommand, yourMovie) {
+
+}
+
+function doWhatCommand(yourCommand) {
 
 }
